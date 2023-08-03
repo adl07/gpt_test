b1efd26f67acc2b9656afaac21ca873dc6f7ba14
Adriel <adrielalegre721@gmail.com>
2023-08-03 17:30:57 -0300

    commit 03-08

--- Services/IOpenAIService.cs
+++ Services/IOpenAIService.cs
@@ -2,7 +2,7 @@ namespace WebApiChatGpt.Services;
 
 public interface IOpenAIService
 {   
-    Task<string>QuestionMedic(string consult);
+    Task<List<string>>QuestionMedic(string consult);
     /*Task<string> CompleteSentence(string text);
     Task<string> CompleteSentenceAdvance(string text);*/
     
--- Services/OpenService.cs
+++ Services/OpenService.cs
@@ -30,7 +30,7 @@ public class OpenService : IOpenAIService
     }
 
     
-    public async Task<string> QuestionMedic(string consult)
+    public async Task<List<string>>QuestionMedic(string consult)
     
     {   
         //SE AGREGA UN TRY&CATCH PARA CAPTURAR ALGUN ERROR QUE PUEDA DARNOS LA API DE OPEN AI AL EJECUTARSE
@@ -49,7 +49,7 @@ public class OpenService : IOpenAIService
                 chat.AppendUserInput(consult);
 
                 //DECLARO EL INSTRUCTIVO DE LA PRIMERA INSTANCIA AL BOT
-                var prompt1 = "Te solicito que identifiques solamente tres paramentros: la(s) especialidad(es) médica(s) en su denominación técnica, el nombre de la ciudad y el nombre del prestador médico o especialista. Por ejemplo, si se trata de un dentista, me gustaría que me proporciones la especialidad de odontología en lugar de usar el término 'dentista'. Si falta el parametro especilidad/es medicas en alguna de las combinaciones agregarle siempre el valor null. Si falta el parametro nombre del 'prestador' médico o médico especialista en alguna de las combinaciones agregarle siempre el valor null. No autocompletes con otros datos. Responde en formato JSON mediante la creación de una lista que se llame 'Prestaciones' y contenga los parametros con sus valores. Recuerda que esos 3 parametros son los que necesito para utilizarlos en mi metodo, por favor siempre identificarlos y diferenciar correctamente a que busqueda representa cada una, por ejemplo si se trata de una consulta: busco un en caba y un pediatra en quilmes, debes saber diferenciar que son dos busquedas dentro de una misma consulta, asi con varias combinaciones. Limitate solamente a responder el JSON, no agregar informacion.";
+                var prompt1 = "Te solicito que identifiques solamente tres paramentros: la(s) especialidad(es) médica(s) en su denominación técnica, el nombre de la ciudad y el nombre del prestador médico o especialista. Por ejemplo, si se trata de un dentista, me gustaría que me proporciones la especialidad de odontología en lugar de usar el término 'dentista'. Si falta el parametro especilidad/es medicas en alguna de las combinaciones agregarle siempre el valor booleano null. Si falta el parametro nombre del 'prestador' médico o médico especialista en alguna de las combinaciones agregarle siempre el valor booleano null. No autocompletes con otros datos. Responde en formato JSON mediante la creación de una lista que se llame 'Prestaciones' y contenga los parametros con sus valores. Por favor siempre identificarlos y diferenciar correctamente a que busqueda pertenece cada parametro, por ejemplo si se trata de una consulta: busco un dentista en boedo y un en lomas de zamora, debes saber diferenciar que son dos busquedas dentro de una misma consulta ya que en una si hay la(s) especialidad(es) médica(s) y en la otra no, por lo cual debes responder el siguiente formato: {Prestaciones: [{Especialidad: odontología,Ciudad: Boedo Prestador: null,Codigo: 11.01},{Especialidad: null,Ciudad: Lomas de Zamora,Prestador: null,Codigo: null}]}, es de importancia que las diferencies correctamente. Limitate solamente a responder el JSON, no agregar informacion.";
 
                 chat.AppendSystemMessage(prompt1);
 
@@ -59,7 +59,7 @@ public class OpenService : IOpenAIService
                 //Console.WriteLine("Respuesta del primer prompt: " + response1);
 
                 //DECLARAMOS EL INSTRUCTIVO DE LA SEGUNDA INSTANCIA AL BOT
-                var prompt2= "Verifica si el valor del parametro 'Especialidad' dentro del objeto"+response1+"se encuentra en el JSON de prestaciones llamado "+_prestaciones+". Si las especialidades se encuentran en el JSON, agrega como parametro 'Codigo' y el valor que solamente tiene en 'CodEspecialidad' y no 'id', sino agregarle null por defecto al parametro 'Codigo'. Manten el mismo formato de respuesta. ";
+                var prompt2= "Verifica si el valor del parametro 'Especialidad' dentro del objeto"+response1+"se encuentra en el JSON de prestaciones llamado "+_prestaciones+". Si las especialidades se encuentran en el JSON, agrega como parametro 'Codigo' y el valor que solamente tiene en 'CodEspecialidad' y no 'id', sino agregarle el valor booleano null por defecto al parametro 'Codigo'. Manten el mismo formato de respuesta. ";
 
                 //EN ESTA INSTANCIA LA APLICA SOBRE LA PRIMERA RESPUESTA
                 chat.AppendSystemMessage(prompt2);
@@ -68,28 +68,28 @@ public class OpenService : IOpenAIService
                 //OBTENEMOS EL RESULTADO FINAL
                 var response2 = await chat.GetResponseFromChatbotAsync();
 
-                //Console.WriteLine("Respuesta del segundo prompt: " + response2);
+                Console.WriteLine("Respuesta del segundo prompt: " + response2);
             
-                //var resultadoDelResponse = respuestaDeConsulta(response2);
+                var resultadoDelResponse = respuestaDeConsulta(response2);
 
-                //return resultadoDelResponse;
+                return resultadoDelResponse;
                 
-                return response2;
+                //return response2;
         }
         catch (Exception ex)
         {
             //Console.WriteLine("Error en la consulta: " + ex.Message);
             
-            return "Error en la consulta: " + ex.Message;
+            //return "Error en la consulta: " + ex.Message;
 
-            //return new List<string>();
+            return new List<string>();
         }
             
     
     }
         
 
-    /*public List<string> respuestaDeConsulta(string json)
+    public List<string> respuestaDeConsulta(string json)
     {
         JObject jsonObject = JObject.Parse(json);
         JArray prestaciones = (JArray)jsonObject["Prestaciones"];
@@ -105,9 +105,9 @@ public class OpenService : IOpenAIService
         foreach (JObject item in prestaciones)
         {
             especialidad = item["Especialidad"].ToString();
-            if(especialidad == null)
+            if (string.IsNullOrEmpty(especialidad))
             {
-                listadoDeResultados.Add("Verificar los datos ingresados son correctos");
+                listadoDeResultados.Add("Verificar si los datos ingresados son correctos");
             }
             else{
                 ubicacion = item["Ciudad"].ToString();
@@ -151,7 +151,6 @@ public class OpenService : IOpenAIService
             Console.WriteLine("Historial:"+"\n" +fecha+"\n"+resultado);
         }
     }
-    */
-    
+
 
 }
